f40985fbbd516aeaea05c6d303df808a9f27d4ac
abhi_ram <abhiramjayapuri2015@gmail.com>
2022-10-19 02:08:28 +0530

    added s17

---
+++ s16/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd $HOME/myexpos/expl
+./expl expl_progs/idle.expl
+./expl expl_progs/odd.expl
+./expl expl_progs/gcd.expl
+./expl expl_progs/sort.expl
+cd $HOME/myexpos/spl
+./spl spl_progs/os_startup.spl
+./spl spl_progs/timer.spl
+./spl spl_progs/int7.spl
+./spl spl_progs/boot_module.spl
+./spl spl_progs/scheduler.spl
+./spl spl_progs/int10.spl
+./spl spl_progs/resource_manager.spl
+./spl spl_progs/device_manager.spl
+./spl spl_progs/console_int_handler.spl
+./spl spl_progs/int6.spl
+cd $HOME/myexpos/xfs-interface
+./xfs-interface run batch
