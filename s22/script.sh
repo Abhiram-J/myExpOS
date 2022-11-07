211a7db040bd4675cbea63d15d4846f86fa21af6
abhi_ram <abhiramjayapuri2015@gmail.com>
2022-11-07 13:57:55 +0530

    added s22

---
+++ s22/script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $HOME/myexpos/expl
+./expl expl_progs/idle.expl
+./expl expl_progs/shell.expl
+./expl expl_progs/rw.expl
+./expl expl_progs/parent.expl
+./expl expl_progs/child.expl
+./expl expl_progs/mergeseq.expl
+./expl expl_progs/mergecnc.expl
+cd $HOME/myexpos/spl
+./spl spl_progs/os_startup.spl
+./spl spl_progs/timer.spl
+./spl spl_progs/int7.spl
+./spl spl_progs/boot_module.spl
+./spl spl_progs/scheduler.spl
+./spl spl_progs/int10.spl
+./spl spl_progs/resource_manager.spl
+./spl spl_progs/device_manager.spl
+./spl spl_progs/console_int_handler.spl
+./spl spl_progs/int6.spl
+./spl spl_progs/int9.spl
+./spl spl_progs/process_manager.spl
+./spl spl_progs/memory_manager.spl
+./spl spl_progs/disk_int_handler.spl
+./spl spl_progs/exception_handler.spl
+./spl spl_progs/int8.spl
+./spl spl_progs/int11.spl
+./spl spl_progs/int13.spl
+./spl spl_progs/int14.spl
+./spl spl_progs/int15.spl
+cd $HOME/myexpos/xfs-interface
+./xfs-interface run batch
